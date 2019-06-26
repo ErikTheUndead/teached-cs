c0f22b99dd0afc695e01a4fe94e4729756e13625
Erik DUHEM <erik.duhem@gmail.com>
2019-06-26 22:53:13 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http.get('api/courses').subscribe(res => console.log(res));
+  }
+}
